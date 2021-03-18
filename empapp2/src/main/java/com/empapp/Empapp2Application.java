7d4c87a74c27e5770a83e1f450ee8f3a592a872e
sravani566 <69153232+sravani566@users.noreply.github.com>
2021-03-18 09:50:20 +0530

    Add files via upload

---
+++ empapp2/src/main/java/com/empapp/Empapp2Application.java
@@ -0,0 +1,37 @@
+package com.empapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.empapp.entities.Employee;
+import com.empapp.service.EmployeeService;
+
+@SpringBootApplication
+public class Empapp2Application implements CommandLineRunner{
+	@Autowired
+	private EmployeeService empService;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Empapp2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		  //System.out.println("rec are saved...");
+		
+		
+		/*
+		 * empService.save(new Employee("Pooja",25)); empService.save(new
+		 * Employee("Sravani", 30)); empService.save(new Employee("Priyanka", 35));
+		 * empService.save(new Employee("Sandhya", 40));
+		 */
+		/* empService.save(new Employee("Pavani", 45)); */
+		 
+		 
+	}
+
+}
